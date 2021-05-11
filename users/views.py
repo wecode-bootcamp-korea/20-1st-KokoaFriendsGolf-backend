e3f0e444f0c9eb59655a50974a48022c2a078ec5
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-11 19:58:23 +0900

    Add: SignUp View

--- users/views.py
+++ users/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import bcrypt
+import jwt
+import json
+from json                   import JSONDecodeError
 
-# Create your views here.
+from django.views           import View
+from django.http            import JsonResponse
+from django.core.exceptions import ValidationError
+
+from users.models           import User
+from utils.validators       import validate_email, validate_password, validate_gender, validate_duplicate, DuplicatedEntryError
+
+class SignUpView(View):
+
+    def post(self, request):
+
+        try:
+            data = json.loads(request.body)
+
+            validate_email(data["email"])
+            validate_password(data["password"])
+            data['gender'] = validate_gender(data.get('gender'))
+            validate_duplicate(User, data)
+
+            hashed_password = bcrypt.hashpw(str(data.get('password')).encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+            user = User.objects.create(
+                email           = data.get('email'),
+                password        = hashed_password,
+                phone_number    = data['phone_number'],
+                name            = data['name'],
+                birthday        = data.get('birthday'),
+                gender          = data.get('gender'),
+            )
+
+            return JsonResponse({"status": "SUCCESS", "user": user.to_dict()}, status=200)
+
+        except JSONDecodeError as e:
+            return JsonResponse({"status": "JSON_DECODE_ERROR", "message": e.msg}, status=400)
+
+        except ValidationError as e:
+            return JsonResponse({"status": "INVALID_DATA_ERROR", "message": e.message}, status=400)
+
+        except KeyError as e:
+            return JsonResponse({"status": "KEY_ERROR", "message": f'Key Error in Field "{e.args[0]}"'}, status=400)
+
+        except DuplicatedEntryError as e:
+            return JsonResponse({"status": "DUPLICATED_ENTRY_ERROR", "message": e.err_message}, status=409)
