b74fa31b1e767e20ca83099ed1f3288fdeda2113
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-11 19:57:50 +0900

    CREATE: Add utils and validators.py

---
+++ utils/validators.py
@@ -0,0 +1,42 @@
+from django.core.exceptions import ValidationError
+
+class DuplicatedEntryError(Exception):
+
+    def __init__(self, duplicated_field):
+        super().__init__()
+        self.err_message = f'Entry {duplicated_field} is duplicated.'
+
+def validate_email(email):
+
+    if not ("@" in email and "." in email):
+        raise ValidationError(f"{email} is not an valid email.")
+
+def validate_password(password):
+    MIN_PASSWORD_LENGTH = 8
+
+    if len(str(password)) < MIN_PASSWORD_LENGTH:
+        raise ValidationError("Your Password is too short. Use Longer Password.")
+
+def validate_gender(gender):
+    if not gender:
+        return 'N'
+
+    if gender not in ['M', 'F']:
+        raise ValidationError("Gender Field should be either 'M' or 'F' or empty.")
+
+    return gender
+
+def validate_duplicate(model, data):
+    non_duplicatable_fields = [
+        _.attname
+        for _ in model._meta.get_fields()
+        if _.unique
+    ]
+
+    for field in non_duplicatable_fields:
+
+        if field in data.keys():
+            field_to_check = {field: data[field]}
+            
+            if model.objects.filter(**field_to_check).exists():
+                raise DuplicatedEntryError(field)
