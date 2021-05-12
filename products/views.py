3d08c5a2fd137b10380ad444fe87c98198ce0ba3
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-12 13:56:51 +0900

    ADD: Product View Skeleton

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views import View
+from products.models import Product
 
-# Create your views here.
+class ProductListView(View):
+    def get(self, request):
+        pass
+
+class ProductDetailView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+class CategoryView(View):
+    def get(self, request):
+        pass
