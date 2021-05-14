19920931d5b67557a7e97c72a8843a2e23648835
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-14 20:54:00 +0900

    MODIFY: User Validator and SignUpView
    - User Validator 부분 User app의 utils.py로 분리
    - email, password validator가 검증된 값 리턴하게 수정, 가독성 고려
    - validate_duplicate 함수 가독성 향상
    - 그 외 코드포매팅

---
+++ users/utils.py
@@ -0,0 +1,24 @@
+from django.core.exceptions import ValidationError
+
+def validate_email(email):
+    if not ("@" in email and "." in email):
+        raise ValidationError(f"{email} is not an valid email.")
+    
+    return email
+
+def validate_password(password):
+    MIN_PASSWORD_LENGTH = 8
+
+    if len(str(password)) < MIN_PASSWORD_LENGTH:
+        raise ValidationError("Your Password is too short. Use Longer Password.")
+    
+    return password
+
+def validate_gender(gender):
+    if not gender:
+        return 'N'
+
+    if gender not in ['M', 'F']:
+        raise ValidationError("Gender Field should be either 'M' or 'F' or empty.")
+
+    return gender
--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 import bcrypt
-import jwt
 import json
 from json                   import JSONDecodeError
 
@@ -8,29 +7,29 @@ from django.http            import JsonResponse
 from django.core.exceptions import ValidationError
 
 from users.models           import User
-from utils.validators       import validate_email, validate_password, validate_gender, validate_duplicate, DuplicatedEntryError
+from users.utils            import validate_email, validate_password, validate_gender
+from utils.validators       import validate_duplicate, DuplicatedEntryError
 
 class SignUpView(View):
-
     def post(self, request):
 
         try:
             data = json.loads(request.body)
 
-            validate_email(data["email"])
-            validate_password(data["password"])
-            data['gender'] = validate_gender(data.get('gender'))
+            email = validate_email(data["email"])
+            password = validate_password(data["password"])
+            gender = validate_gender(data.get('gender'))
             validate_duplicate(User, data)
-
-            hashed_password = bcrypt.hashpw(str(data.get('password')).encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-
+            
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            
             user = User.objects.create(
-                email           = data.get('email'),
+                email           = email,
                 password        = hashed_password,
                 phone_number    = data['phone_number'],
                 name            = data['name'],
                 birthday        = data.get('birthday'),
-                gender          = data.get('gender'),
+                gender          = gender,
             )
 
             return JsonResponse({"status": "SUCCESS", "user": user.to_dict()}, status=200)
