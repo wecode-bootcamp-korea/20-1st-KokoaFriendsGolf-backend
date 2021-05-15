a5520efd4f38409915c55ac35c8e42b2658ab6b3
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-15 21:19:12 +0900

    ADD: ProductDetailView Logic

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.views import View
+from django.http.response import JsonResponse
+from django.views    import View
+
 from products.models import Product
 
 class ProductListView(View):
@@ -6,12 +8,15 @@ class ProductListView(View):
         pass
 
 class ProductDetailView(View):
-    def get(self, request):
-        pass
+    def get(self, request, id):
+        try:
+            product = Product.objects.get(id=id)
+            product_info = product.get_info()
+            return JsonResponse({'status': "SUCCESS", 'data':{'product': product_info}}, status=200)
 
-    def post(self, request):
-        pass
-
+        except Product.DoesNotExist:
+            return JsonResponse({"status": "PRODUCT_NOT_FOUND", "message": "존재하지 않는 상품입니다."}, status=404)
+            
 class CategoryView(View):
     def get(self, request):
         pass
