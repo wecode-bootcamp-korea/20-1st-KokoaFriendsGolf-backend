58fed7ace9aede6e5804babcff0fc3a126de0fc0
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2021-05-16 23:51:36 +0900

    MODIFY: modify product_list_view

--- products/views.py
+++ products/views.py
@@ -7,122 +7,117 @@ from .models                  import (Category, SubCategory, Product, Character,
 
 class ProductListView(View):
     def get(self, request):
-        sub_category__id = request.GET.get("scid", 0)
-        category__id     = request.GET.get("cid", 0)
+        ordering       = request.GET.get("sort", 0)
+        category__name = request.GET["cname"]
 
-        all_category     = Category.objects.all()
-        category_id_list = []
+        all_category       = Category.objects.all()
+        category_name_list = []
         for categorys in all_category:
-            category_id = categorys.id
-            category_id_list.append(category_id)
+            category_name  = categorys.name
+            category_name_list.append(category_name)
 
-        all_sub_category     = SubCategory.objects.all()
-        sub_category_id_list = []
+        all_sub_category       = SubCategory.objects.all()
+        sub_category_name_list = []
         for sub_categorys in all_sub_category:
-            sub_category_id = sub_categorys.id
-            sub_category_id_list.append(sub_category_id)
+            sub_category_name  = sub_categorys.name
+            sub_category_name_list.append(sub_category_name)
 
-        if int(category__id) in category_id_list:
+        if category__name in category_name_list: 
+            category__id  = Category.objects.get(name=category__name).id
             category_all = SubCategory.objects.filter(category_id=category__id)
             category_products_list=[]
             for category_products in category_all:
                 category_products_all = category_products.product_set.all()
-                for category_product in category_products_all:
-                    product_info = {
-                       "id"            : category_product.id,
-                       "name"          : category_product.name,
-                       "price"         : float(category_product.price),
-                       "thumbnail_url" : category_product.thumbnail_url,
-                       "is_new"        : False if not category_product.is_new else True,
-                       "is_sale"       : False if not category_product.is_sale else True,
-                       "is_soldout"    : False if not category_product.is_soldout else True,
-                       "is_set"        : False if not category_product.is_set else True,
-                       "discount_ratio": float(category_product.discount_ratio),
-                       "created_at"    : category_product.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                       "updated_at"    : category_product.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
-                       }
+                if ordering == 'high_price':
+                    a = category_products_all.order_by('-price')
+                if ordering == 'low_price':
+                    a = category_products_all.order_by('price')
+                if ordering == 0:
+                    a = category_products_all.order_by('created_at')
+                for category_product in a:
+                    product_info = category_product.get_info()
+                    del (product_info['contents'],
+                         product_info['is_picked'],
+                         product_info['subcategory'],
+                         product_info['character'],
+                         product_info['category'])
                     category_products_list.append(product_info)
             return JsonResponse({"products_list": category_products_list}, status=200)
 
-        if int(sub_category__id) in sub_category_id_list:
+        if category__name in sub_category_name_list:
+            sub_category__id = SubCategory.objects.get(name=category__name).id
             sub_category = SubCategory.objects.get(id=sub_category__id)
             sub_category_products = sub_category.product_set.all()
             sub_category_products_list = []
-            for sub_category_product in sub_category_products:
-                product_info = {
-                 "id"            : sub_category_product.id,
-                 "name"          : sub_category_product.name,
-                 "price"         : float(sub_category_product.price),
-                 "thumbnail_url" : sub_category_product.thumbnail_url,
-                 "is_new"        : False if not sub_category_product.is_new else True,
-                 "is_sale"       : False if not sub_category_product.is_sale else True,
-                 "is_soldout"    : False if not sub_category_product.is_soldout else True,
-                 "is_set"        : False if not sub_category_product.is_set else True,
-                 "discount_ratio": float(sub_category_product.discount_ratio),
-                 "created_at"    : sub_category_product.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                 "updated_at"    : sub_category_product.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
-                 }
+            if ordering == 'high_price':
+                a = sub_category_products.order_by('-price')
+            if ordering == 'low_price':
+                a = sub_category_products.order_by('price')
+            if ordering == 0:
+                a = sub_category_products.order_by('created_at')
+            for sub_category_product in a:
+                product_info = sub_category_product.get_info()
+                del (product_info['contents'],
+                     product_info['is_picked'],
+                     product_info['subcategory'],
+                     product_info['character'],
+                     product_info['category'])
                 sub_category_products_list.append(product_info)
             return JsonResponse({"products_list": sub_category_products_list}, status=200)
 
-        if category__id == 0 and sub_category__id == 0:
+        if category__name =='0':
             products = Product.objects.all()
-            products_list=[]
-            for product in products:
-                product_info = {
-                 "id"            : product.id,
-                 "name"          : product.name,
-                 "price"         : float(product.price),
-                 "thumbnail_url" : product.thumbnail_url,
-                 "is_new"        : False if not product.is_new else True,
-                 "is_sale"       : False if not product.is_sale else True,
-                 "is_soldout"    : False if not product.is_soldout else True,
-                 "is_set"        : False if not product.is_set else True,
-                 "discount_ratio": float(product.discount_ratio),
-                 "created_at"    : product.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                 "updated_at"    : product.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
-                 } 
+            products_list = []
+            if ordering == 'high_price':
+                a = products.order_by('-price')
+            if ordering == 'low_price':
+                a = products.order_by('price')
+            if ordering == 0:
+                a = products.order_by('created_at')
+            for product in a:
+                product_info = product.get_info()
+                del (product_info['contents'],
+                     product_info['is_picked'],
+                     product_info['subcategory'],
+                     product_info['character'],
+                     product_info['category'])
                 products_list.append(product_info)
             return JsonResponse({"products_list": products_list}, status=200)
 
-        if int(category__id) not in category_id_list or int(sub_category__id) not in sub_category_id_list:
-            return JsonResponse ({"message": "NO CATEGORY ID"}, status=400)           
+        if category__name not in category_name_list:
+            return JsonResponse ({"message": "NO CATEGORY"}, status=404)           
 
 class CharacterListView(View):
     def get(self, request):
-        character__id    = request.GET.get('chr_id',0)
+        character__name = request.GET['cname']
+        ordering        = request.GET.get('sort', 0)
 
-        all_character_id = Character.objects.all()
-        character_id_list = []
-        for characters in all_character_id:
-            character_id = characters.id
-            character_id_list.append(character_id)
+        all_character = Character.objects.all()
+        character_name_list = []
+        for characters in all_character:
+            character_name = characters.name
+            character_name_list.append(character_name)
 
-        if int(character__id) in character_id_list:
-            character_all          = Character.objects.filter(id=character__id)
+        if character__name in character_name_list:
+            one_character          = Character.objects.get(name=character__name)
             character_product_list = []
-            for one_character in character_all:
-                character_products = one_character.product_set.all()
-                for character_product in character_products:
-                    product_info = {
-                     "id"            : character_product.id,
-                     "name"          : character_product.name,
-                     "price"         : float(character_product.price),
-                     "thumbnail_url" : character_product.thumbnail_url,
-                     "is_new"        : False if not character_product.is_new else True,
-                     "is_sale"       : False if not character_product.is_sale else True,
-                     "is_soldout"    : False if not character_product.is_soldout else True,
-                     "is_set"        : False if not character_product.is_set else True,
-                     "discount_ratio": float(character_product.discount_ratio),
-                     "created_at"    : character_product.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                     "updated_at"    : character_product.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
-                     } 
-                    character_product_list.append(product_info)
+            character_products = one_character.product_set.all()
+            if ordering == 'high_price':
+                a = character_products.order_by('-price')
+            if ordering == 'low_price':
+                a = character_products.order_by('price')
+            if ordering == 0:
+                a = character_products.order_by('created_at')
+            for character_product in a:
+                product_info = character_product.get_info()
+                del (product_info['contents'],
+                     product_info['is_picked'],
+                     product_info['subcategory'],
+                     product_info['character'],
+                     product_info['category'])
+                character_product_list.append(product_info)
             return JsonResponse({"products_list": character_product_list}, status=200)
 
-        if int(character__id) not in character_id_list:
-            return JsonResponse({"message": "NO CHARACTER ID"}, status=400)
+        if character__name not in character_name_list:
+            return JsonResponse({"message": "NO CHARACTER"}, status=404)
 
-class SortView(View):
-    def get(self, request):
-        pass
