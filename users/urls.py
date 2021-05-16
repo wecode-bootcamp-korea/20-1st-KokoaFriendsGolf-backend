77be1aa5b656669ff8689e78a3b4200a5c082b09
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-16 20:08:12 +0900

    FIX: User Urls.py Typo

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from users.views import SignInView, SignUpView
 
 urlpatterns = [
-    path('/users/signin', SignInView.as_view()),
-    path('/users/signup', SignUpView.as_view()),
+    path('/signin', SignInView.as_view()),
+    path('/signup', SignUpView.as_view()),
 ]
