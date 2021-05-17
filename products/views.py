17d4c76a920e6680a1eaf89193427c3122fb47a1
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-17 13:23:29 +0900

    ADD: ProductListView Logic

--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
-from django.http.response import JsonResponse
-from django.views    import View
+from django.http.response   import JsonResponse
+from django.views           import View
 
-from products.models import Product
+from products.models        import (Category, 
+                                    SubCategory, 
+                                    Product, 
+                                    Character)
+from utils.utils            import get_name_list
 
 class ProductListView(View):
     def get(self, request):
@@ -20,3 +24,45 @@ class ProductDetailView(View):
 class CategoryView(View):
     def get(self, request):
         pass
+
+class ProductListView(View):
+    def get(self, request):
+        order_by        = request.GET.get('orderBy', '-RECENT')
+        cname           = request.GET.get('cname')
+        products        = Product.objects.none()
+
+        if cname is None:
+            products = Product.objects.all()
+        
+        if cname in get_name_list(Category):
+            category      = Category.objects.get(name=cname)
+            subcategories = SubCategory.objects.filter(category=category)
+            products      = Product.objects.filter(subcategory__in = subcategories)
+
+        if cname in get_name_list(SubCategory):
+            products = Product.objects.filter(subcategory__name = cname)
+            
+        if cname in get_name_list(Character):
+            products = Product.objects.filter(charater__name = cname)
+        
+        if order_by == 'RECENT':
+            products = products.order_by('-created_at')
+            
+        if order_by == '-RECENT':
+            products = products.order_by('created_at')
+            
+        if order_by == 'PRICE':
+            products = products.order_by('-price')
+            
+        if order_by == '-PRICE':
+            products = products.order_by('price')
+            
+        if order_by == 'LIKE':
+            products = sorted(products, key = lambda product: product.like_users.count(), reverse=True)
+
+        if order_by == '-LIKE':
+            products = sorted(products, key = lambda product: product.like_users.count())
+
+        data = [product.get_info(exclude=["contents"]) for product in products]
+
+        return JsonResponse({"status": "SUCCESS", "data": data}, status=200)
