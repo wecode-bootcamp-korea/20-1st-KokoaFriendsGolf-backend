5c1125a471fb2186967c39d0605919fe26886909
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-18 19:51:10 +0900

    ADD: check_user Decorator in ProductDetailView

--- products/views.py
+++ products/views.py
@@ -1,17 +1,20 @@
-from django.http.response import JsonResponse
-from django.views    import View
+from django.http.response       import JsonResponse
+from django.views               import View
+from django.utils.decorators    import method_decorator
 
-from products.models import Product
-
+from products.models            import Product
+from utils.decorators           import check_user
 class ProductListView(View):
     def get(self, request):
         pass
 
 class ProductDetailView(View):
+    @method_decorator(check_user())
     def get(self, request, id):
         try:
             product      = Product.objects.get(id=id)
-            product_info = product.get_info()
+            user         = request.user
+            product_info = product.get_info(user=user)
             return JsonResponse({'status': "SUCCESS", 'data':{'product': product_info}}, status=200)
 
         except Product.DoesNotExist:
