99e859377f66a8ea627d58bc0a23ed90198840fd
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-19 16:18:08 +0900

    ADD: Patch Method to ProductDetailView

--- products/views.py
+++ products/views.py
@@ -3,7 +3,8 @@ from django.views               import View
 from django.utils.decorators    import method_decorator
 
 from products.models            import Product
-from utils.decorators           import check_user
+from utils.decorators           import check_user, login_required
+
 class ProductListView(View):
     def get(self, request):
         pass
@@ -19,7 +20,24 @@ class ProductDetailView(View):
 
         except Product.DoesNotExist:
             return JsonResponse({"status": "PRODUCT_NOT_FOUND", "message": "존재하지 않는 상품입니다."}, status=404)
-            
+    
+    @method_decorator(login_required())
+    def patch(self, request, id):
+        try:
+            product  = Product.objects.get(id=id)
+            user     = request.user
+            is_liked = product.get_info(user=user)['is_liked']
+
+            if is_liked:
+                product.like_users.remove(user)
+            else:
+                product.like_users.add(user)
+
+            return JsonResponse({'status': "SUCCESS", 'message': f'is_liked changed to {product.get_info(user=user)["is_liked"]}'}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"status": "PRODUCT_NOT_FOUND", "message": "존재하지 않는 상품입니다."}, status=404)
+
 class CategoryView(View):
     def get(self, request):
         pass
