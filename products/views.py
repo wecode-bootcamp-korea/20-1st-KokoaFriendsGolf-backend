4290b1399caa7d03bd0a445c67a72eff5ba4fbfb
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2021-05-20 20:32:31 +0900

    MODIFY: code_formatting

--- products/views.py
+++ products/views.py
@@ -1,27 +1,27 @@
-from django.http.response import JsonResponse
-from django.views import View
+from django.http.response    import JsonResponse
+from django.views            import View
 from django.utils.decorators import method_decorator
 
-from products.models import Product, Category, SubCategory, Character
-from utils.decorators import check_user, login_required
-from utils.utils import get_name_list
+from products.models         import Product, Category, SubCategory, Character
+from utils.decorators        import check_user, login_required
+from utils.utils             import get_name_list
 
 
 class ProductListView(View):
-    LIMIT_DEFAULT = 16
+    LIMIT_DEFAULT  = 16
     OFFSET_DEFAULT = 0
 
     def get(self, request):
-        order_by = request.GET.get('orderBy', '-RECENT')
-        cname = request.GET.get('cname')
-        search = request.GET.get('search')
-        limit = int(request.GET.get('limit', self.LIMIT_DEFAULT))
-        offset = int(request.GET.get('offset', self.OFFSET_DEFAULT))
-        products = Product.objects.none()
+        order_by         = request.GET.get('orderBy', '-RECENT')
+        cname            = request.GET.get('cname')
+        search           = request.GET.get('search')
+        limit            = int(request.GET.get('limit', self.LIMIT_DEFAULT))
+        offset           = int(request.GET.get('offset', self.OFFSET_DEFAULT))
+        products         = Product.objects.none()
         all_product_name = get_name_list(Product)
-        product_list = []
-        is_last_page = True
-        user = request.user
+        product_list     = []
+        is_last_page     = True
+        user             = request.user
 
         if cname in [None, '']:
             products = Product.objects.all()
@@ -43,23 +43,21 @@ class ProductListView(View):
             products = Product.objects.filter(name__in=product_list)
 
         query_dict = {
-            'RECENT': '-created_at',
+            'RECENT' : '-created_at',
             '-RECENT': 'created_at',
-            'PRICE': '-price',
-            '-PRICE': 'price'
+            'PRICE'  : '-price',
+            '-PRICE' : 'price'
         }
         products = products.order_by(query_dict[order_by])
 
         if order_by == 'LIKE':
-            products = sorted(
-                products, key=lambda product: product.like_users.count(), reverse=True)
+            products = sorted(products, key=lambda product: product.like_users.count(), reverse=True)
 
         if order_by == '-LIKE':
-            products = sorted(
-                products, key=lambda product: product.like_users.count())
+            products = sorted(products, key=lambda product: product.like_users.count())
 
         if len(products) > (offset+limit):
-            products = products[offset:offset+limit]
+            products     = products[offset:offset+limit]
             is_last_page = False
         else:
             products = products[offset:]
@@ -74,8 +72,8 @@ class ProductDetailView(View):
     @method_decorator(check_user())
     def get(self, request, id):
         try:
-            product = Product.objects.get(id=id)
-            user = request.user
+            product      = Product.objects.get(id=id)
+            user         = request.user
             product_info = product.get_info(user=user)
             return JsonResponse({'status': "SUCCESS", 'data': {'product': product_info}}, status=200)
 
@@ -85,8 +83,8 @@ class ProductDetailView(View):
     @method_decorator(login_required())
     def patch(self, request, id):
         try:
-            product = Product.objects.get(id=id)
-            user = request.user
+            product  = Product.objects.get(id=id)
+            user     = request.user
             is_liked = product.get_info(user=user)['is_liked']
 
             if is_liked:
@@ -98,8 +96,3 @@ class ProductDetailView(View):
 
         except Product.DoesNotExist:
             return JsonResponse({"status": "PRODUCT_NOT_FOUND", "message": "존재하지 않는 상품입니다."}, status=404)
-
-
-class CategoryView(View):
-    def get(self, request):
-        pass
