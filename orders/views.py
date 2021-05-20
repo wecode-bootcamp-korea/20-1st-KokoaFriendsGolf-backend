30864efd768c96ad62e598c03ca790fdc41a1d8e
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-20 19:37:58 +0900

    MODIFY: Apply Reviewer's Suggestions
    - Code 포매팅
    - .filter 메서드 이어서 쓰던 것 한번에 여러 인자 넣도록 변경
    - Order Types 한 곳으로 정리
    - GET 메서드 Response의 date 값을 Order 테이블의 updated_at으로 세팅
      (PURCHASED로 변경된 시각을 주문 내역 페이지에서 알 수 있다.)

--- orders/views.py
+++ orders/views.py
@@ -1,113 +1,108 @@
 import json
-from json import JSONDecodeError
+from json                           import JSONDecodeError
 
-from django.views import View
-from django.utils.decorators import method_decorator
-from django.http.response import JsonResponse
+from django.views                   import View
+from django.utils.decorators        import method_decorator
+from django.http.response           import JsonResponse
 
-from products.models import Product
-from orders.models import Order, OrderList, OrderStatus
-from utils.decorators import check_user, login_required
-
+from products.models                import Product
+from orders.models                  import Order, OrderList, OrderStatus
+from utils.decorators               import login_required
 
 class OrderListView(View):
-    ORDER_TYPES = ["IN_CART", "PURCHASE_INSTANT"]
+    ALL_ORDER_TYPES       = ["IN_CART", "PURCHASE_INSTANT", "PURCHASE_CART", "PURCHASED"]
+    POSTABLE_ORDER_TYPES  = ["IN_CART", "PURCHASE_INSTANT"]
+    PATCHEABLE_ORDER_TYPE = ["PURCHASE_CART", "PURCHASED"]
 
     @method_decorator(login_required())
     def get(self, request):
-        user = request.user
-        order_type = request.GET.get('orderType')
-        order_list = []
-        sum_total_origin_price = 0
+        user                       = request.user
+        order_type                 = request.GET.get('orderType')
+        order_list                 = []
+        sum_total_origin_price     = 0
         sum_total_discounted_price = 0
-        sum_total_discount = 0
+        sum_total_discount         = 0
 
-        if order_type not in self.ORDER_TYPES:
-            return JsonResponse({"status": "INVALID_ORDER_TYPE_ERROR"}, status=401)
+        if order_type not in self.ALL_ORDER_TYPES:
+            return JsonResponse({"status": "ORDER_TYPE_ERROR", "message": f'Order type should be one of {self.ALL_ORDER_TYPES}'}, status=400)
 
-        order_items = OrderList.objects.filter(
-            order__user=user, order__order_status__status=order_type)
+        order_items = OrderList.objects.filter(order__user=user, order__order_status__status=order_type)
 
         for product_info in order_items:
             order_product = {
-                "order_id": product_info.id,
-                "date": '2020-05-20',
-                "name": product_info.product.name,
-                "quantity": product_info.quantity,
-                "origin_price": float(product_info.product.price),
-                "discounted_price": float(product_info.product.price - (product_info.product.price * product_info.product.discount_ratio)),
-                "thumbnail_url": product_info.product.thumbnail_url,
-                "discount_ratio": float(product_info.product.discount_ratio),
-                "total_discounted_price": float((product_info.product.price - (product_info.product.price * product_info.product.discount_ratio)) * product_info.quantity),
-                "total_price": float(product_info.product.price * product_info.quantity),
-                "total_discount": float(product_info.product.price * product_info.product.discount_ratio * product_info.quantity)
+                "order_id"              : product_info.id,
+                "date"                  : product_info.order.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
+                "name"                  : product_info.product.name,
+                "quantity"              : product_info.quantity,
+                "origin_price"          : float(product_info.product.price),
+                "discounted_price"      : float(product_info.product.price * (1 - product_info.product.discount_ratio)),
+                "thumbnail_url"         : product_info.product.thumbnail_url,
+                "discount_ratio"        : float(product_info.product.discount_ratio),
+                "total_discounted_price": float((product_info.product.price * (1 - product_info.product.discount_ratio)) * product_info.quantity),
+                "total_price"           : float(product_info.product.price * product_info.quantity),
+                "total_discount"        : float(product_info.product.price * product_info.product.discount_ratio * product_info.quantity)
             }
             order_list.append(order_product)
 
         for order_list_dict in order_list:
-            sum_total_origin_price += order_list_dict["total_price"]
+            sum_total_origin_price     += order_list_dict["total_price"]
             sum_total_discounted_price += order_list_dict["total_discounted_price"]
-            sum_total_discount += order_list_dict["total_discount"]
+            sum_total_discount         += order_list_dict["total_discount"]
 
         return JsonResponse(data={
             "status": "SUCCESS",
             "data": {
-                "sum_total_origin_price": sum_total_origin_price,
+                "sum_total_origin_price"    : sum_total_origin_price,
                 "sum_total_discounted_price": sum_total_discounted_price,
-                "sum_total_discount": sum_total_discount,
-                "final_price": sum_total_discounted_price + 3000,
-                "shipping_fee": 3000,
-                "product_list": order_list,
-                "user_info": {
-                    "id": user.id,
-                    "name": user.name,
-                    "email": user.email,
-                    "phone_number": user.phone_number,
-                }
+                "sum_total_discount"        : sum_total_discount,
+                "final_price"               : sum_total_discounted_price + 3000,
+                "shipping_fee"              : 3000,
+                "product_list"              : order_list,
+                "user_info"                 : {
+                                                "id"          : user.id,
+                                                "name"        : user.name,
+                                                "email"       : user.email,
+                                                "phone_number": user.phone_number,
+                                              }
             }
         }, status=200)
 
     @method_decorator(login_required())
     def post(self, request):
-        POSTABLE_ORDER_TYPES = ["IN_CART", "PURCHASE_INSTANT"]
         try:
-            data = json.loads(request.body)
-            user = request.user
+            data            = json.loads(request.body)
+            user            = request.user
             order_item_list = data['order_list']
-            order_type = data['order_type']
+            order_type      = data['order_type']
 
             if order_item_list == []:
                 return JsonResponse({"status": "INVALID_ORDER", "message": f'Request must contain at least one order.'}, status=400)
 
-            if order_type not in POSTABLE_ORDER_TYPES:
-                return JsonResponse({"status": "ORDER_TYPE_ERROR", "message": f'Order type should be one of {POSTABLE_ORDER_TYPES}'}, status=400)
+            if order_type not in self.POSTABLE_ORDER_TYPES:
+                return JsonResponse({"status": "ORDER_TYPE_ERROR", "message": f'Order type should be one of {self.POSTABLE_ORDER_TYPES}'}, status=400)
 
-            self.init_order_types(POSTABLE_ORDER_TYPES)
+            self.init_order_types(self.POSTABLE_ORDER_TYPES)
             self.reset_orders(user)
 
             order_status = OrderStatus.objects.get(status=order_type)
 
             for order_item in order_item_list:
-                quantity = order_item['quantity']
+                quantity   = order_item['quantity']
                 product_id = order_item['product_id']
-                product = Product.objects.get(id=product_id)
-                new_order = Order.objects.create(
-                    receiver_name=user.name, user=user, order_status=order_status)
-                if OrderList.objects.filter(order__user=user).filter(product=product).filter(order__order_status__status=order_type).exists():
-                    existing_order = OrderList.objects.filter(order__user=user)\
-                        .filter(product=product)\
-                        .filter(order__order_status__status=order_type)[0]
-                    existing_order_info = existing_order.order
+                product    = Product.objects.get(id=product_id)
+                new_order  = Order.objects.create(receiver_name=user.name, user=user, order_status=order_status)
+                if OrderList.objects.filter(order__user=user, product=product, order__order_status__status=order_type).exists():
+                    existing_order = OrderList.objects.filter(order__user=user, product=product, order__order_status__status=order_type)[0]
+                    existing_order_info     = existing_order.order
                     existing_order.quantity = quantity
-                    existing_order.order = new_order
+                    existing_order.order    = new_order
                     existing_order.save()
                     existing_order_info.delete()
 
                 else:
-                    OrderList.objects.create(
-                        quantity=quantity, product=product, order=new_order)
+                    OrderList.objects.create(quantity=quantity, product=product, order=new_order)
 
-            return JsonResponse({"status": "SUCCESS", "data": {"order_list": self.get_order_info(order_type_list=POSTABLE_ORDER_TYPES, user=user)}}, status=200)
+            return JsonResponse({"status": "SUCCESS", "data": {"order_list": self.get_order_info(order_type_list=self.POSTABLE_ORDER_TYPES, user=user)}}, status=200)
 
         except JSONDecodeError as e:
             return JsonResponse({"status": "JSON_DECODE_ERROR", "message": e.msg}, status=400)
@@ -120,36 +115,30 @@ class OrderListView(View):
 
     @method_decorator(login_required())
     def patch(self, request):
-        PATCHEABLE_ORDER_TYPE = ["PURCHASE_CART", "PURCHASED"]
-
         try:
-            data = json.loads(request.body)
-            user = request.user
+            data       = json.loads(request.body)
+            user       = request.user
             order_type = data['order_type']
 
-            if order_type not in PATCHEABLE_ORDER_TYPE:
-                return JsonResponse({"status": "ORDER_TYPE_ERROR", "message": f'Order type should be one of {PATCHEABLE_ORDER_TYPE}'}, status=400)
+            if order_type not in self.PATCHEABLE_ORDER_TYPE:
+                return JsonResponse({"status": "ORDER_TYPE_ERROR", "message": f'Order type should be one of {self.PATCHEABLE_ORDER_TYPE}'}, status=400)
 
-            self.init_order_types(PATCHEABLE_ORDER_TYPE)
+            self.init_order_types(self.PATCHEABLE_ORDER_TYPE)
 
-            user_order_list = OrderList.objects.filter(order__user=user)
+            user_order_list    = OrderList.objects.filter(order__user=user)
             order_transit_list = OrderList.objects.none()
 
             if order_type == 'PURCHASE_CART':
-                order_id_list = data['order_id_list']
-                order_transit_list = user_order_list.filter(
-                    id__in=order_id_list)
+                order_id_list      = data['order_id_list']
+                order_transit_list = user_order_list.filter(id__in=order_id_list)
 
             if order_type == 'PURCHASED':
-                order_transit_list = user_order_list.filter(order__order_status__status__in=[
-                                                            "PURCHASE_CART", "PURCHASE_INSTANT"])
+                order_transit_list = user_order_list.filter(order__order_status__status__in=["PURCHASE_CART", "PURCHASE_INSTANT"])
 
             for order in order_transit_list:
-                order_info = order.order
-                order_info.order_status = OrderStatus.objects.get(
-                    status=order_type)
+                order_info              = order.order
+                order_info.order_status = OrderStatus.objects.get(status=order_type)
                 order_info.save()
-
             return JsonResponse({"status": "SUCCESS", "data": {"order_list": self.get_order_info(order_type_list=[order_type], user=user)}}, status=200)
 
         except KeyError as e:
@@ -168,11 +157,9 @@ class OrderListView(View):
                 order.order.delete()
 
             if order_status.status == 'PURCHASE_CART':
-                order_info = order.order
-                order_info.order_status = OrderStatus.objects.get(
-                    status='IN_CART')
+                order_info              = order.order
+                order_info.order_status = OrderStatus.objects.get(status='IN_CART')
                 order_info.save()
-
         return
 
     def init_order_types(self, order_type_list):
@@ -183,22 +170,19 @@ class OrderListView(View):
 
     def get_order_info(self, order_type_list=[], user=None):
         if user:
-            order_list = OrderList.objects.filter(order__user=user)\
-                                          .filter(order__order_status__status__in=order_type_list)
+            order_list = OrderList.objects.filter(order__user=user, order__order_status__status__in=order_type_list)
         else:
-            order_list = OrderList.objects.filter(
-                order__order_status__status__in=order_type_list)
+            order_list = OrderList.objects.filter(order__order_status__status__in=order_type_list)
 
         order_info_list = []
 
         for order in order_list:
             order_info = {
-                "id": order.id,
+                "id"          : order.id,
                 "product_name": order.product.name,
-                "quantity": order.quantity,
-                "user": order.order.user.name,
-                "status": order.order.order_status.status,
+                "quantity"    : order.quantity,
+                "user"        : order.order.user.name,
+                "status"      : order.order.order_status.status,
             }
             order_info_list.append(order_info)
-
         return order_info_list
